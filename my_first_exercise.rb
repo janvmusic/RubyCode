6c021cd3dd2cee4f768f9192a5eedf96b1471ddc
jnunez <jnunez@switchfly.com>
2017-11-21 14:36:25 -0700

    Update tab space value

--- my_first_exercise.rb
+++ my_first_exercise.rb
@@ -1,22 +1,22 @@
 #!/usr/bin/env ruby
 class Warrior
-    def initialize(name)
-        @name = name
-    end
+  def initialize(name)
+    @name = name
+  end
 
-    def about
-        puts "#{@name} is a Warrior"
-    end
+  def about
+    puts "#{@name} is a Warrior"
+  end
 end
 
 class Timelord
-    def initialize(name)
-        @name = name
-    end
+  def initialize(name)
+    @name = name
+  end
 
-    def about
-        puts "#{@name} is a Timelord"
-    end
+  def about
+    puts "#{@name} is a Timelord"
+  end
 end
 
 player_1 = Warrior.new("John Wick")
