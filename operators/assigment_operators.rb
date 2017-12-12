8a5d1914353cf13b732974ddd4e23f98302167e0
jnunez <jnunez@switchfly.com>
2017-12-11 17:15:58 -0700

    Add main operator exercise

---
+++ operators/assigment_operators.rb
@@ -0,0 +1,41 @@
+my_number = 100
+puts "===== Addition ====="
+puts my_number = my_number + 1
+
+my_number = 100
+puts my_number += 1
+
+puts "===== Substraction ====="
+my_number = 100
+puts my_number = my_number - 1
+
+my_number = 100
+puts my_number -= 1
+
+puts "===== Mutiplication ====="
+my_number = 100
+puts my_numer = my_number * 2
+
+my_number = 100
+puts my_number *= 2
+
+puts "===== Division ====="
+my_number = 100
+puts my_number / 2
+
+my_number = 100
+puts my_number /= 2
+
+puts "===== Module ====="
+my_number = 100
+puts my_number % 10
+
+my_number = 100
+puts my_number %= 10
+
+puts "===== Exponent ====="
+my_number = 100
+puts my_number ** 10
+
+my_number = 100
+puts my_number **= 10
